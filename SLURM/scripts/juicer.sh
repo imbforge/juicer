930f3b73d9bcab83a92dd72affaa62e7e4875d45
Patrick Hüther <huether@imb-mainz.de>
2024-08-09 14:32:46 +0200

    drop comment containing indirect expansion of null variable

--- SLURM/scripts/juicer.sh
+++ SLURM/scripts/juicer.sh
@@ -927,9 +927,6 @@ DEDUPGUARD`
         fi 
 	squeue -u $USER -o "%A %T %j %E %R" | column -t
 	awk -v queue=$long_queue -v groupname=$groupname -v debugdir=$debugdir -v dir=$outputdir -v topDir=$topDir -v juicedir=$juiceDir -v site=$site -v genomeID=$genomeID -v genomePath=$genomePath -v user=$USER -v guardjid=$guardjid -v justexact=$justexact -f $juiceDir/scripts/split_rmdups.awk $outputdir/merged_sort.txt
-	##Schedule new job to run after last dedup part:
-	##Push guard to run after last dedup is completed:
-	##srun --ntasks=1 -c 1 -p "$queue" -t 1 -o ${debugdir}/dedup_requeue-%j.out -e ${debugdir}/dedup-requeue-%j.err -J "$groupname_msplit0" -d singleton echo ID: $ echo "\${!SLURM_JOB_ID}"; scontrol update JobID=$guardjid dependency=afterok:\$SLURM_JOB_ID
 	squeue -u $USER -o "%A %T %j %E %R" | column -t
 	date
 	
